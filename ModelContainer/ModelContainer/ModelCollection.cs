a3cec4ca5108e528042dd08cf4b3ba39441670eb
Morten Frederiksen <mrtn.frederiksen@gmail.com>
2015-10-22 16:28:30 +0200

    Add solution

---
+++ ModelContainer/ModelContainer/ModelCollection.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ModelContainer
+{
+  public class ModelCollection
+  {
+    private Dictionary<Type, object> models = new Dictionary<Type, object>();
+
+    public void AddModel<T>(T t)
+    {
+      models.Add(t.GetType(), t);
+    }
+
+    public T GetModel<T>()
+    {
+      return (T)models[typeof(T)];
+    }
+  }
+}
